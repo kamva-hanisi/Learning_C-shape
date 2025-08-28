f47bacae03a69c29e948c513a0403d7540b9fd0c
KAMVA <lucashanisi@gmail.com>
2025-08-28 17:56:22 +0200

    Add .NET 9.0 project and console application
    Created `02_Variables.csproj` for a .NET 9.0 executable project with implicit usings and nullable reference types. Added `02_Variables.sln` for Visual Studio project configuration. Introduced `Variable.cs` with a console application that interacts with the user to collect and display their name, age, and height.

---
+++ 02_Variables/Variable.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your height: ");
+            double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Hello, I am {name}, {age} years old, and I'm {height} meters tall.");
+
+            Console.ReadLine();
+        }
+    }
+}
